025ab83daa699897520a39ed80bdafb9ac565331
John Pham <johnpham7496@gmail.com>
2020-08-18 20:41:36 -0700

    making a failing test

--- tests/app_test.py
+++ tests/app_test.py
@@ -5,4 +5,4 @@ def test_tc001_hello():
     response = my_app.test_client().get('/')
 
     assert response.status_code == 200
-    assert response.data == b'Johnny like pizza'
+    assert response.data == b'Johnny like pizzdasdw'
