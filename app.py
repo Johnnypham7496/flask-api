661cf3a7360b67621c7e939160fddd2d56d3ab1c
John Pham <johnpham7496@gmail.com>
2020-08-18 21:20:00 -0700

    added new secret end point and test

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def hello_world():
     return 'Johnny like pizza'
 
 
+@my_app.route('/secret')
+def hello_world():
+    return 'Get outta here. Its my secret'
+
+
 if __name__ == '__main__':
     my_app.run(host='0.0.0.0', port=5000, debug=True)
--- tests/app_test.py
+++ tests/app_test.py
@@ -3,6 +3,11 @@ from app import my_app
 
 def test_tc001_hello():
     response = my_app.test_client().get('/')
-
     assert response.status_code == 200
     assert response.data == b'Johnny like pizza'
+
+
+def test_tc002_secret():
+    response = my_app.test_client().get('/secret')
+    assert response.status_code == 200
+    assert response.data == b'Get outta here. Its my secret'
